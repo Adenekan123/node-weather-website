d9a0f6849b26c710926dbee9ccffd920ff266d1f
Adenekan Usman <mr.nekan123@gmail.com>
2021-04-07 20:18:36 +0100

    Remove unnecessary space

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
 const searchForm = document.querySelector('form');
 const search = document.querySelector('input');
 
-
 searchForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     if(search !== null && search.value === '' || search.value === null) return alert('Please Enter A Location');
